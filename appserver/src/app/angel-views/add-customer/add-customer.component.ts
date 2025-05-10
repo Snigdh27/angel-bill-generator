862e9fda5ab6a97ee7a0423031caccdab82e1a86
Snigdh <snigdh.kulshreshtha@nyalazone.com>
2025-05-10 22:44:41 +0530

    add customer validation added

--- appserver/src/app/angel-views/add-customer/add-customer.component.ts
+++ appserver/src/app/angel-views/add-customer/add-customer.component.ts
@@ -1,19 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-add-customer',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './add-customer.component.html',
   styleUrl: './add-customer.component.css'
 })
 export class AddCustomerComponent {
 
   customer_form!: FormGroup;
+  states: any = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+  ]
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private toastr: ToastrService
   ){}
 
   ngOnInit() {
@@ -24,19 +56,28 @@ export class AddCustomerComponent {
     this.customer_form = this.fb.group({
       customer_type: ['Individual', Validators.required],
       name: ['', Validators.required],
-      mobile_number: ['', [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]],
-      email_address: ['', [Validators.required, Validators.email]],
+      mobile_number: ['', [Validators.required]],
+      email_address: [''],
       billing_address: ['', Validators.required],
       gstin_no: [''],
       state: ['', Validators.required],
-      state_code: ['', Validators.required]
+      state_code: ['']
     });
   }
 
+  validate_customer(){
+    return this.customer_form.valid;
+  }
+
   create_customer() {
-    console.log(this.customer_form.value);
-    this.addToLocalStorageList("customer_list_data", this.customer_form.value);
-    this.customer_form.disable();
+    console.log(this.customer_form);
+    if(this.validate_customer()){
+      this.addToLocalStorageList("customer_list_data", this.customer_form.value);
+      this.customer_form.disable();
+      this.toastr.success("Customer Created Successfully");
+    } else{
+      this.toastr.error("Please fill all the required fields");
+    }
   }
   
   addToLocalStorageList(key: string, newEntry: any) {
